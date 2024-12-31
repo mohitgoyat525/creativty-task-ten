1ed1bd4204971940cf92dc9420a49971c0816863
mohitgoyat525 <montyits71@gmail.com>
2024-12-31 09:50:19 +0530

    add-changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,28 +2,38 @@ const containers = document.querySelectorAll(".img-container");
 let currentIndex = 0;
 
 function showSlide(index) {
-  containers.forEach((container, i) => {
+  for (let i = 0; i < containers.length; i++) {
+    const container = containers[i];
     gsap.to(container, {
-      left: i === index ? "0%" : i < index ? "-100%" : "100%",
+      left: i === index ? "0%" : i < index ? "-100%" : i === containers.length - 1 ? "0%" : "100%",
       opacity: i === index ? 1 : 0,
       duration: 1.5,
       ease: "expo.inOut",
     });
 
+    const headingText = container.querySelector(".heading-text");
     if (i === index) {
-      gsap.to(container.querySelector(".heading-text"), {
+      gsap.to(headingText, {
         opacity: 1,
         duration: 0.5,
         delay: 0.3,
       });
+    } else {
+      gsap.to(headingText, {
+        opacity: 0,
+        duration: 0.5,
+        delay: 0,
+      });
     }
-  });
+  }
 }
 
 function startSlideshow() {
   showSlide(currentIndex);
-  currentIndex = (currentIndex + 1) % containers.length;
-  setTimeout(startSlideshow, 3000);
+  if (currentIndex < containers.length - 1) {
+    currentIndex++;
+    setTimeout(startSlideshow, 1000); 
+  }
 }
 
 startSlideshow();
